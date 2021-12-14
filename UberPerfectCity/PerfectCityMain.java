33d9bee423edcb5bbcbaa74f60a080b58935c0cf
Elbert Lucas <elbertlucas0@gmail.com>
2021-12-14 17:43:54 -0300

    Launcher Sequence Checker inicializado

--- UberPerfectCity/PerfectCityMain.java
+++ UberPerfectCity/PerfectCityMain.java
@@ -4,8 +4,8 @@ import static UberPerfectCity.PerfectCity.calculatePerfectCity;
 
 public class PerfectCityMain {
     public static void main(String[] args) {
-        double [] departure = {9, 7};
-        double [] destination = { 0.2, 2};
+        double [] departure = {2.4, 1};
+        double [] destination = {5, 7.3};
         double distance = calculatePerfectCity(departure, destination);
 
         System.out.println(distance);
